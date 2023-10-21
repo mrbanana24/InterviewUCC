2e198d77517b11fb3b68484a130f8fda8a0b9407
mrbanana24 <franpercivaldi@gmail.com>
2023-10-21 16:07:14 -0300

    change width in login screen

--- front/src/screens/Login/index.jsx
+++ front/src/screens/Login/index.jsx
@@ -1,47 +1,47 @@
-import { Grid } from '@giphy/react-components'
-import MuiGrid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import LoginForm from '../../components/LoginForm'
-import { GiphyFetch } from '@giphy/js-fetch-api';
+import { Grid } from "@giphy/react-components";
+import MuiGrid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import LoginForm from "../../components/LoginForm";
+import { GiphyFetch } from "@giphy/js-fetch-api";
 
 const Login = () => {
   const giphyFetch = new GiphyFetch("jG4BYKxt5QNeDVk0ufYIiYjU1t7lGGGe");
-  const searchTag = 'Welcome back';
+  const searchTag = "Welcome back";
   const fetchGifs = (offset) => giphyFetch.search(searchTag, { offset, limit: 10 });
 
   const blurStyle = {
-    position: 'relative',
-    overflow: 'hidden',
+    position: "relative",
+    overflow: "hidden",
   };
 
   const overlayStyle = {
-    content: '',
-    position: 'absolute',
+    content: "",
+    position: "absolute",
     top: 0,
     left: 0,
-    width: '100%',
-    height: '100%',
-    background: 'rgba(255, 255, 255, 0.1)',
+    width: "100%",
+    height: "100%",
+    background: "rgba(255, 255, 255, 0.1)",
     zIndex: 1,
   };
 
   return (
-    <MuiGrid container sx={{ backgroundColor:'#8FBC8F'}}>
+    <MuiGrid container sx={{ backgroundColor: "#8FBC8F" }}>
       {/* First half screen with blur and elevation effect */}
-      <MuiGrid item xs={12} md={6} sx={{ maxHeight: '100vh', overflow: 'hidden' }}>
-        <Paper elevation={3} style={{ ...blurStyle, position: 'relative', overflow: 'hidden' }}>
+      <MuiGrid item xs={12} md={6} sx={{ maxHeight: "100vh", overflow: "hidden" }}>
+        <Paper elevation={3} style={{ ...blurStyle, position: "relative", overflow: "hidden" }}>
           <div style={overlayStyle}></div>
-          <Grid width={1500} columns={5} gutter={2} borderRadius={9} fetchGifs={fetchGifs} key={searchTag} />
+          <Grid width={1501} columns={5} gutter={2} borderRadius={9} fetchGifs={fetchGifs} key={searchTag} />
         </Paper>
       </MuiGrid>
       {/* Second half screen */}
-      <MuiGrid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <MuiGrid item xs={12} md={6} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <Paper elevation={3}>
           <LoginForm />
         </Paper>
       </MuiGrid>
     </MuiGrid>
-  )
-}
+  );
+};
 
 export default Login;
